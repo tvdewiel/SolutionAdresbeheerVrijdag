e196929eff141a3594fb0a9584b34d6534054f9b
Tom Vande Wiele <tvdewiel@hotmail.com>
2023-10-20 11:06:42 +0200

    Add project files.

---
+++ AdresbeheerREST/Controllers/AdresbeheerController.cs
@@ -0,0 +1,22 @@
+﻿using AdresbeheerREST.Model.Output;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdresbeheerREST.Controllers
+{
+    [Route("api/[controller]/gemeente")]
+    [ApiController]
+    public class AdresbeheerController : ControllerBase
+    {
+        [HttpGet("{gemeenteId}")]
+        public ActionResult<GemeenteRESToutputDTO> GetGemeente(int gemeenteId)
+        {
+            try
+            {
+                return Ok(); 
+                //vraag gemeente aan BL
+                //gemeente omzetten naar DTO
+            }
+            catch (Exception ex) { return NotFound(ex.Message); }
+    }
+}
